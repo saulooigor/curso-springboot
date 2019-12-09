5c9b7164a9363611373370a9ab29a781a6c2ebbc
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-09 16:26:28 -0300

    Create Order Part 1

--- src/main/java/com/example/vazco/curso/entities/User.java
+++ src/main/java/com/example/vazco/curso/entities/User.java
@@ -1,13 +1,20 @@
 package com.example.vazco.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +26,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy="client")
+	private List<Order> orders = new ArrayList<Order>();
 
 	public User() {
 
@@ -71,6 +81,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
