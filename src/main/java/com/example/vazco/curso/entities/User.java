f587fd8add7c94502317dc61f5d0a5c2c68e46d0
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-09 14:48:25 -0300

    JPA e banco de dados H2

--- src/main/java/com/example/vazco/curso/entities/User.java
+++ src/main/java/com/example/vazco/curso/entities/User.java
@@ -2,10 +2,18 @@ package com.example.vazco.curso.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -89,7 +97,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
