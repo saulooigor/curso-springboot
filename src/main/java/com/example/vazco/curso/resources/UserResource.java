7e812e99c6ddf949a5c71c695f806b6310a42e00
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-09 16:05:30 -0300

    Create findAll and findById

--- src/main/java/com/example/vazco/curso/resources/UserResource.java
+++ src/main/java/com/example/vazco/curso/resources/UserResource.java
@@ -1,22 +1,35 @@
 package com.example.vazco.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.vazco.curso.entities.User;
+import com.example.vazco.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Maria", "maria@gmail.com", "98765412", "12345");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
-	
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User u = service.findById(id);
+		return ResponseEntity.ok().body(u);
+
+	}
 
 }
