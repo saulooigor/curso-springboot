aad81b250ea8ac7311243d8a1eeedf198032364c
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-10 16:17:17 -0300

    Product-OrderItem association

--- src/main/java/com/example/vazco/curso/entities/OrderItem.java
+++ src/main/java/com/example/vazco/curso/entities/OrderItem.java
@@ -10,42 +10,42 @@ import com.example.vazco.curso.entities.pk.OrderItemPK;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name="tb_order_item")
+@Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
-		
+
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
-		
+
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -90,7 +90,5 @@ public class OrderItem implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
