56ac0734cd3f201ffd7ce346a582274119705740
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-10 14:09:00 -0300

    Many To many association join table

--- src/main/java/com/example/vazco/curso/entities/Category.java
+++ src/main/java/com/example/vazco/curso/entities/Category.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -25,7 +26,8 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 
-	@Transient
+	@ManyToMany(mappedBy = "categories")
+	@JsonIgnore
 	private Set<Product> products = new HashSet<>();
 
 	public Category() {
