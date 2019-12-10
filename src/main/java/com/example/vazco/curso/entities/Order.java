cc7d88c1bacb32719c8e46b2ec97c2abfec32219
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-10 18:17:43 -0300

    Subtotal e TotalMethods

--- src/main/java/com/example/vazco/curso/entities/Order.java
+++ src/main/java/com/example/vazco/curso/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 		return items;
 	}
 
+	public Double getTotal() {
+		Double soma = 0.0;
+		for (OrderItem x : items) {
+			soma += x.getSubTotal();
+		}
+		return soma;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
