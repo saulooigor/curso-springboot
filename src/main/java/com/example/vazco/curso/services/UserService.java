0294abb7adbcc8e4769533f13da44d409525bc1f
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-11 18:01:42 -0300

    Exception delete

--- src/main/java/com/example/vazco/curso/services/UserService.java
+++ src/main/java/com/example/vazco/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.vazco.curso.entities.User;
 import com.example.vazco.curso.repositories.UserRepository;
+import com.example.vazco.curso.services.exceptions.DataBaseException;
 import com.example.vazco.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,11 +33,21 @@ public class UserService {
 		return repository.save(obj);
 
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+
+		}catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+			
+		}
+
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
@@ -45,7 +58,7 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 
 }
