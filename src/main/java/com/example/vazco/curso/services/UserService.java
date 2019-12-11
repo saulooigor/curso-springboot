3d72ce724f869484ef2741ffb6ecbfb3f1cf2875
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-11 18:08:20 -0300

    Update exception

--- src/main/java/com/example/vazco/curso/services/UserService.java
+++ src/main/java/com/example/vazco/curso/services/UserService.java
@@ -3,6 +3,8 @@ package com.example.vazco.curso.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,17 +43,24 @@ public class UserService {
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataBaseException(e.getMessage());
-			
+
 		}
 
 	}
 
 	public User update(Long id, User obj) {
+		try {
+		
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
-		return repository.save(entity);
+		return repository.save(entity);	
+		
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+			
+		}
 	}
 
 	private void updateData(User entity, User obj) {
