14a0fa0ac51e03ecfc0fb851b8b7e6e02c9584bf
Saulo Igor <saulo.igor.alves@gmail.com>
2019-12-11 17:42:09 -0300

    Exception findById

--- src/main/java/com/example/vazco/curso/services/UserService.java
+++ src/main/java/com/example/vazco/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.vazco.curso.entities.User;
 import com.example.vazco.curso.repositories.UserRepository;
+import com.example.vazco.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
 	}
 
